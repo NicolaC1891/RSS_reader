be5b519981519bd2a80a317b4fc740f7567a21d5
NicolaC <nicolassummoner@mail.ru>
2025-06-10 21:06:35 +0300

    Hello, Vladimir!

--- rss_reader.py
+++ rss_reader.py
@@ -2,21 +2,22 @@ import aiohttp
 import asyncio
 
 # RSS feed URL
-bbc_url = "http://feeds.bbci.co.uk/news/world/europe/rss.xml"
+urls = ["http://feeds.bbci.co.uk/news/world/europe/rss.xml", "https://www.onliner.by/feed"]
 
 # Define core function
-async def main():
+async def main(urls):
+    for url in urls:
     # Create session
-    async with aiohttp.ClientSession() as session:
-        # Send request, get reply as response, wrap in context
-        async with session.get(bbc_url) as response:
-            print("Connection status code:", response.status)
-            # Headers are response metadata (coding, content type, length etc.), stored in dict-like object
-            print("Content type:", response.headers['content-type'])
-            # Assign contents to variable
-            content = await response.text()
-            print("First 30 characters:", content[:30])
-            print(content)    # This is to be parsed
+        async with aiohttp.ClientSession() as session:
+            # Send request, get reply as response, wrap in context
+            async with session.get(url) as response:
+                print("Connection status code:", response.status)
+                # Headers are response metadata (coding, content type, length etc.), stored in dict-like object
+                print("Content type:", response.headers['content-type'])
+                # Assign contents to variable
+                content = await response.text()
+                print("First 30 characters:", content[:30])
+                print(content)    # This is to be parsed
 
 # Starting main asynchronous script
-asyncio.run(main())
+asyncio.run(main(urls))
