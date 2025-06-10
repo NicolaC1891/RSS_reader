713ccd9402634db31a63227c64c29e2134b77b92
NicolaC <nicolassummoner@mail.ru>
2025-06-10 22:04:08 +0300

    Refactored reader into class

--- rss_reader.py
+++ rss_reader.py
@@ -1,23 +1,20 @@
 import aiohttp
 import asyncio
 
-# RSS feed URL
 urls = ["http://feeds.bbci.co.uk/news/world/europe/rss.xml", "https://www.onliner.by/feed"]
 
-# Define core function
-async def main(urls):
-    for url in urls:
-    # Create session
-        async with aiohttp.ClientSession() as session:
-            # Send request, get reply as response, wrap in context
-            async with session.get(url) as response:
-                print("Connection status code:", response.status)
-                # Headers are response metadata (coding, content type, length etc.), stored in dict-like object
-                print("Content type:", response.headers['content-type'])
-                # Assign contents to variable
-                content = await response.text()
-                print("First 30 characters:", content[:30])
-                print(content)    # This is to be parsed
 
-# Starting main asynchronous script
+class RssReader:
+    async def get_xml(self, url):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                content = await response.text()
+                print(content)
+
+
+async def main(urls):
+    reader = RssReader()
+    for url in urls:
+        await reader.get_xml(url)
+
 asyncio.run(main(urls))
